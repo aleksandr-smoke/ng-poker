8c52df39bb65ce1d1cbf527080320cff30627a46
Dimov Aleksandr Viktorovich <a.dimov@tinkoff.ru>
2023-09-18 15:54:32 +0300

    add taiga-ui to project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,25 @@
+import { NgDompurifySanitizer } from "@tinkoff/ng-dompurify";
+import { TuiRootModule, TuiDialogModule, TuiAlertModule, TUI_SANITIZER, TuiButtonModule } from "@taiga-ui/core";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+    ],
+    imports: [
+        BrowserModule,
+        BrowserAnimationsModule,
+        TuiRootModule,
+        TuiDialogModule,
+        TuiAlertModule,
+        TuiButtonModule,
+    ],
+    providers: [
+        { provide: TUI_SANITIZER, useClass: NgDompurifySanitizer },
+    ],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}

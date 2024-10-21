900d17e796ced6abc58d24bd01f7ab03cef076da
Alexander <alexdimow@yandex.ru>
2024-10-22 01:15:14 +0300

    migrate to standalone components

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,13 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { AppModule } from './app/app.module';
 
-platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.error(err));
+import { NG_EVENT_PLUGINS } from '@taiga-ui/event-plugins';
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { AppComponent } from './app/app.component';
+
+bootstrapApplication(AppComponent, {
+    providers: [
+        provideAnimations(),
+        NG_EVENT_PLUGINS,
+    ]
+}).catch(err => console.error(err));
